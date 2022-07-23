66ac04b5b76ff3c670a348b5ffcecc9578ed01ae
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-07-23 18:46:18 +0300

    Done

--- iTunesSearch/Controllers/StoreItemListTableViewController.swift
+++ iTunesSearch/Controllers/StoreItemListTableViewController.swift
@@ -2,9 +2,7 @@
 import UIKit
 
 class StoreItemListTableViewController: UITableViewController {
-            
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
